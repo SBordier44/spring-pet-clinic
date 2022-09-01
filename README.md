b94df77096ee0f362795c82b621908231a3efb4e
Sylvain BORDIER <sbordier@nbb-lease.fr>
2022-09-01 20:01:42 +0200

    Spring project initialization

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-pet-clinic
+
 Spring Pet Clinic with Java 11 / Maven 3 / Spring Framework 5
---
+++ src/test/java/fr/nubox/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nubox.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
