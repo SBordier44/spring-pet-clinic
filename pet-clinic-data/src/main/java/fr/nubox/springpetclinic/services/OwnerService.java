84b059493ba5d8cdfaa9f13ad20eae2d92042712
Sylvain BORDIER <slye@nubox.fr>
2022-09-03 22:19:18 +0200

    Refactoring to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/fr/nubox/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/fr/nubox/springpetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package fr.nubox.springpetclinic.services;
 
 import fr.nubox.springpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-    Owner findById(Long id);
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/fr/nubox/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/fr/nubox/springpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package fr.nubox.springpetclinic.services;
 
 import fr.nubox.springpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
