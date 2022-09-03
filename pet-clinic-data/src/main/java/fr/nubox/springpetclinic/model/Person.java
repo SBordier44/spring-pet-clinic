0a9e5a78391e785edacfe3b8ecce097fbbfc8b88
Sylvain BORDIER <slye@nubox.fr>
2022-09-03 17:08:59 +0200

    Adding base entity with ID. Closes #10.

--- pet-clinic-data/src/main/java/fr/nubox/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/fr/nubox/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package fr.nubox.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/fr/nubox/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/fr/nubox/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package fr.nubox.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
