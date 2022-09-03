555bf4bdd50b39a74e9c3cc63d81f1ec8d0086db
Sylvain BORDIER <slye@nubox.fr>
2022-09-03 16:56:08 +0200

    Implement interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/fr/nubox/springpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package fr.nubox.springpetclinic.services;
+
+import fr.nubox.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/fr/nubox/springpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package fr.nubox.springpetclinic.services;
+
+import fr.nubox.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
